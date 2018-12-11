4fe6758ec7c0dcc17c4147f3d347565b80ba2584
Clarke Williams <cpv.williams@hotmail.com>
2018-12-11 18:27:39 +0000

    Initial project setup
    Test 1

---
+++ Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assessment 2 - Data driven app
+//
+//  Created by Clarke Williams on 11/12/2018.
+//  Copyright © 2018 Clarke Williams. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
