2d21b6d3d0b1b7699e080bd31759a93ae5fa4069
Clarke Williams <cpv.williams@hotmail.com>
2019-01-15 18:49:17 +0000

    Update Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp

--- Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp
+++ Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp
@@ -36,7 +36,7 @@ int main() {
     }
     tweets.close(); // closes the file
     
-    //------------------ Searching for money
+    //------------------ Searching and counting for money
     
     tweets.open("sampleTweets.csv"); // opens the sample tweets
     if(tweets.good()){ // checks to see if the file has opened
@@ -50,6 +50,90 @@ int main() {
             }
             
 
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
+    //------------------ Searching and counting for Politics
+    
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("Politics")<=currentTweets.length()){ // if the word 'Politics' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
+    //------------------ Searching and counting for Paris
+    
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("Paris")<=currentTweets.length()){ // if the word 'Paris' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
+    //------------------ Searching and counting for DreamWorks
+    
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("DreamWorks")<=currentTweets.length()){ // if the word 'DreamWorks' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
+    //------------------ Searching and counting for Uber
+    
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("Uber")<=currentTweets.length()){ // if the word 'Uber' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
         }
         
     }else {
