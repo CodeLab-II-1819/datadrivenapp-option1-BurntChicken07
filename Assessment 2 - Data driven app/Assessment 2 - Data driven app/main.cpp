168a4659b7f3e2d3339b609e9656c4e4446f58d5
Clarke Williams <cpv.williams@hotmail.com>
2019-01-15 18:33:43 +0000

    First commit

--- Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp
+++ Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp
@@ -7,9 +7,57 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    
+    string currentTweets;
+    int totalTweets = 0;
+    
+    ifstream tweets;
+    
+    //------ Opening and counting the tweets
+    
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        cout << "Files open successfully" << endl; // prints if it has opened successfully
+    
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            cout << currentTweets << endl; // prints the tweets held
+            totalTweets++; // counts
+        }
+        cout <<"The total number of tweets are: " << totalTweets << endl;
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
+    //------------------ Searching for money
+    
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("money")<=currentTweets.length()){ // if the word 'money' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
     return 0;
+        
 }
+
