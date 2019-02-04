a2977b4ef676a01d448f01ce3de09deedf8180f3
Clarke Williams <cpv.williams@hotmail.com>
2019-02-04 21:40:16 +0000

    Update Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp

--- Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp
+++ Assessment 2 - Data driven app/Assessment 2 - Data driven app/main.cpp
@@ -13,12 +13,46 @@ using namespace std;
 
 int main() {
     
+    int input = 0;
+    
+    cout << "Enter 1 to count tweets" << endl;
+    cin >> input;
+    
+    cout << "Enter 2 to count tweets mentioning money" << endl;
+    cin >> input;
+    
+    cout << "Enter 3 to count tweets mentioning politics" << endl;
+    cin >> input;
+    
+    cout << "Enter 4 to count tweets mentioning Paris" << endl;
+    cin >> input;
+    
+    cout << "Enter 5 to count tweets mentioning Dreamworks" << endl;
+    cin >> input;
+    
+    cout << "Enter 6 to count tweets mentioning Uber" << endl;
+    cin >> input;
+    
+    cout << "Enter 7 to count tweets mentioning Tends" << endl;
+    cin >> input;
+    
+    cout << "Enter 8 to count tweets mentioning President" << endl;
+    cin >> input;
+    
+    cout << "Enter 9 to count tweets mentioning Celebrity" << endl;
+    cin >> input;
+    
+    cout << "Enter 10 to count tweets mentioning Apple" << endl;
+    cin >> input;
+    
     string currentTweets;
     int totalTweets = 0;
     
     ifstream tweets;
     
     //------ Opening and counting the tweets
+    
+    if (input == 1){
     
     tweets.open("sampleTweets.csv"); // opens the sample tweets
     if(tweets.good()){ // checks to see if the file has opened
@@ -35,9 +69,13 @@ int main() {
         cout << "The files havent opened" << endl; // prints if the files havent opened
     }
     tweets.close(); // closes the file
+        
+    }
     
     //------------------ Searching and counting for money
-    
+
+    else if (input == 2){
+        
     tweets.open("sampleTweets.csv"); // opens the sample tweets
     if(tweets.good()){ // checks to see if the file has opened
         
@@ -57,8 +95,11 @@ int main() {
     }
     tweets.close(); // closes the file
     
+    }
     //------------------ Searching and counting for Politics
-    
+
+    else if (input == 3){
+        
     tweets.open("sampleTweets.csv"); // opens the sample tweets
     if(tweets.good()){ // checks to see if the file has opened
         
@@ -77,8 +118,11 @@ int main() {
         cout << "The files havent opened" << endl; // prints if the files havent opened
     }
     tweets.close(); // closes the file
+    }
     
     //------------------ Searching and counting for Paris
+
+    else if (input == 4){
     
     tweets.open("sampleTweets.csv"); // opens the sample tweets
     if(tweets.good()){ // checks to see if the file has opened
@@ -99,8 +143,12 @@ int main() {
     }
     tweets.close(); // closes the file
     
-    //------------------ Searching and counting for DreamWorks
+    }
     
+    //------------------ Searching and counting for DreamWorks
+
+    else if (input == 5){
+        
     tweets.open("sampleTweets.csv"); // opens the sample tweets
     if(tweets.good()){ // checks to see if the file has opened
         
@@ -119,9 +167,13 @@ int main() {
         cout << "The files havent opened" << endl; // prints if the files havent opened
     }
     tweets.close(); // closes the file
+        
+    }
     
     //------------------ Searching and counting for Uber
-    
+
+    else if (input == 6){
+        
     tweets.open("sampleTweets.csv"); // opens the sample tweets
     if(tweets.good()){ // checks to see if the file has opened
         
@@ -141,7 +193,107 @@ int main() {
     }
     tweets.close(); // closes the file
     
+    }
+    //------------------ Searching and counting for Trends
+
+    else if (input == 7){
+        
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("Trends")<=currentTweets.length()){ // if the word 'Trends' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+        
+    }
+    
+    //------------------ Searching and counting for President
+
+    else if (input == 8){
+        
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("President")<=currentTweets.length()){ // if the word 'President' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+        
+    }
+    
+    //------------------ Searching and counting for Celebrity
+
+    else if (input == 9){
+        
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("Celebrity")<=currentTweets.length()){ // if the word 'Celebrity' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
+    }
+        
+    //------------------ Searching and counting for Apple
+
+    else if (input == 10){
+        
+    tweets.open("sampleTweets.csv"); // opens the sample tweets
+    if(tweets.good()){ // checks to see if the file has opened
+        
+        while(!tweets.eof()){ // while the program is at the end of the file (eof), do this loop
+            getline(tweets, currentTweets); // goes through each 63,693 tweets, get 1 line per run through, saves the line to this variable called 'currentTweets'
+            
+            if (currentTweets.find("Apple")<=currentTweets.length()){ // if the word 'Apple ' is found before the length of the tweet do this
+                cout << currentTweets << endl;
+                
+            }
+            
+            
+        }
+        
+    }else {
+        cout << "The files havent opened" << endl; // prints if the files havent opened
+    }
+    tweets.close(); // closes the file
+    
+    }
+        
     return 0;
         
 }
-
